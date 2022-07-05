8849aacf674d644a3abaf786d241fe71220d74fe
Robert <adrianrobert288@gmail.com>
2022-07-05 17:19:54 -0300

    Injetado dados na lista da view

--- recipe/views.py
+++ recipe/views.py
@@ -1,10 +1,14 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def index(request):
-    return render(request, 'recipe/pages/index.html')
+    return render(request, 'recipe/pages/index.html', context={
+        'recipes': [make_recipe() for _ in range(10)]
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipe/pages/recipe-view.html')
+    return render(request, 'recipe/pages/recipe-view.html', context={
+        'recipe': make_recipe()
+    })
