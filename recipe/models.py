f1dbc8f39f61a1a59c941dd0b961acf9d073b3a4
Robert <adrianrobert288@gmail.com>
2022-07-06 18:51:19 -0300

    Voltado a aplicação a uma versão anterior
    	- Devido a um erro onde os nome de uma tabela eram iguais,
    	- decidi por voltar para um versão onde essas tabelas ainda
    	- não tinham sido criadas.
    
    	- Criado 2 tabelas em models.py
    	- Adicionado essas 2 tabela em admin.py para serem manipuladas
    	  através do sistema de admin do Django

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=200)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_step = models.TextField()
+    preparation_step_is_html = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    cover = models.ImageField(upload_to='recipe/cover/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
