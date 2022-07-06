05307e02d72a2ca64e5ac74be7095e0b4b64a00e
Robert <adrianrobert288@gmail.com>
2022-07-06 16:58:13 -0300

    Criado: app name e removido urls feitas em hardcode

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from recipe import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.index),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.index, name='home'),
+    path('recipes/<int:id>/', views.recipe, name='recipe'),
 ]
