b2e19c6a42e055a6f9e07b75881081eaebb35079
Robert <adrianrobert288@gmail.com>
2022-07-07 15:23:58 -0300

    Puxando os dados do nosso model

--- recipe/views.py
+++ recipe/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
+from .models import Recipe
+
 
 def index(request):
+    recipe = Recipe.objects.all().order_by('-id')
     return render(request, 'recipe/pages/index.html', context={
-        'recipes': [make_recipe() for _ in range(10)]
+        'recipes': recipe
+    })
+
+
+def category(request, category_id):
+    recipe = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipe/pages/index.html', context={
+        'recipes': recipe
     })
 
 
