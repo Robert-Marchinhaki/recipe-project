4d2b9bef64a8c507b554153112d44275721dd867
Robert <adrianrobert288@gmail.com>
2022-07-07 20:14:07 -0300

    Atualizado recipe no painel admin
    	- Agora é possível ver mais campos de informações
    	- É possíve utilizar filtros
    	- Definido ação para alterar o estado da publicação

--- recipe/admin.py
+++ recipe/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from .models import Category, Recipe
 
 
+@admin.action(description="Aprovar publicação")
+def published_on(modeladmin, request, queryset):
+    queryset.update(is_published=True)
+
+
+@admin.action(description="Desaprovar publicação")
+def published_off(modeladmin, request, queryset):
+    queryset.update(is_published=False)
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     pass
@@ -10,4 +20,19 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'title',
+        'is_published',
+        'created_at',
+    ]
+    list_filter = [
+        'created_at',
+        'preparation_step_is_html',
+    ]
+    actions = [
+        published_on,
+        published_off,
+    ]
+
+
+# admin.site.register(RecipeAdmin)
