5c8d141805b90c1b2160052b3c43ff6cfa717b7a
Robert <adrianrobert288@gmail.com>
2022-07-11 20:48:37 -0300

    Criado fixtures para um teste único
    	- Recipe, Category, Author

--- recipe/tests/test_recipe_views.py
+++ recipe/tests/test_recipe_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
 from recipe import views
+from recipe.models import Category, Recipe, User
 
 
 class RecipesViewsTest(TestCase):
@@ -22,7 +23,31 @@ class RecipesViewsTest(TestCase):
             reverse('recipes:home')).content.decode('utf-8')
         self.assertIn('id="dont-have-recipe"', response)
 
+    def test_recipe_home_template_loads_recipe(self):
+        category = Category.objects.create(name='Category')
+        user = User.objects.create_user(
+            first_name='name',
+            last_name='lastname',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            title='Recipe Title',
+            description='Recipe description',
+            slug='Slug field',
+            preparation_time=2,
+            preparation_time_unit='minutos',
+            servings=5,
+            servings_unit='pessoas',
+            preparation_step='Passos para fazer a receita',
+            preparation_step_is_html=False,
+            is_published=True,
+        )
+        test = 1
+
     # category test
+
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func, views.category)
