d22b0169800be0fed117fd22d45a9cb2f136a0d3
Robert <adrianrobert288@gmail.com>
2022-07-12 15:08:44 -0300

    Criado métodos de teste para detail e category
    	- Esses métodos testam se a receita está sendo carregada

--- recipe/tests/test_recipe_views.py
+++ recipe/tests/test_recipe_views.py
@@ -18,6 +18,7 @@ class RecipesViewsTest(RecipeBaseTest):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipe/pages/index.html')
 
+    # @skip('WIP')
     def test_recipe_home_view_no_have_recipes(self):
         response = self.client.get(
             reverse('recipes:home')).content.decode('utf-8')
@@ -25,8 +26,12 @@ class RecipesViewsTest(RecipeBaseTest):
             '<h1>Nenhuma receita foi publicada ou aprovada.</h1>',
             response)
 
-    def test_recipe_home_template_loads_recipe(self):
+        # self.fail('WIP')
+
+    def test_recipe_home_template_loads_recipes(self):
+        # need a recipe for this test
         self.create_recipe()
+
         response = self.client.get(reverse('recipes:home'))
         context = response.context['recipes'].first()
         content = response.content.decode('utf-8')
@@ -45,6 +50,19 @@ class RecipesViewsTest(RecipeBaseTest):
         ).status_code
         self.assertEqual(response, 404)
 
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'This is a category test'
+
+        # need a recipe for this test
+        self.create_recipe(title=needed_title)
+
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1}))
+        content = response.content.decode('utf-8')
+
+        # Testing if the title was rendered
+        self.assertIn(needed_title, content)
+
     # detail test
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
@@ -54,3 +72,16 @@ class RecipesViewsTest(RecipeBaseTest):
         response = self.client.get(
             reverse('recipes:recipe', kwargs={'id': 1000})).status_code
         self.assertEqual(response, 404)
+
+    def test_recipe_detai_template_loads_recipe(self):
+        needed_title = 'This is a detail page - Test if loads one recipe'
+
+        # need a recipe for this test
+        self.create_recipe(title=needed_title)
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1}))
+
+        content = response.content.decode('utf-8')
+
+        # Testing if the title was rendered
+        self.assertIn(needed_title, content)
