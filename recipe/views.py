9531f42b05b424ae1976a6ff2f0279b7ff81a5cb
Robert <adrianrobert288@gmail.com>
2022-07-27 17:26:23 -0300

    Corrigido bugs e criado um novo método
    	- Ocorria um bug quando o final_page era menor que o stop_range
    	- Esse bug fazia com que a paginação não fosse demonstrada
    	  corretamente
    	- Criado método make_pagination para reaproveitamento de código

--- recipe/views.py
+++ recipe/views.py
@@ -1,8 +1,7 @@
-from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
-from utils.test_func_pagination import make_pagination_range
+from utils.test_func_pagination import make_pagination
 
 from .models import Recipe
 
@@ -10,19 +9,7 @@ from .models import Recipe
 def index(request):
     recipe = Recipe.objects.filter(is_published=True).order_by('-id')
 
-    try:
-        current_page = int(request.GET.get('page', 9))
-    except ValueError:
-        current_page = 1
-    pagination = Paginator(recipe, 3)
-    pag_get_page = pagination.get_page(current_page)
-
-    pagination_range = make_pagination_range(
-        pagination.page_range,
-        4,
-        current_page,
-    )
-
+    pag_get_page, pagination_range = make_pagination(request, recipe, 3)
     return render(request, 'recipe/pages/index.html', context={
         'recipes': pag_get_page,
         'pagination_range': pagination_range
@@ -35,16 +22,16 @@ def category(request, category_id):
             category__id=category_id,
             is_published=True,
         ).order_by('-id'))
-
+    pag_get_page, pagination_range = make_pagination(request, recipe, 3)
     return render(request, 'recipe/pages/category.html', context={
-        'recipes': recipe,
+        'recipes': pag_get_page,
+        'pagination_range': pagination_range,
         'title': f'{recipe[0].category.name} - Category | '
     })
 
 
 def recipe(request, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True)
-
     return render(request, 'recipe/pages/recipe-view.html', context={
         'recipe': recipe,
         'detail_page': True
@@ -63,9 +50,11 @@ def search(request):
         ),
         is_published=True
     ).order_by('-id')
-
+    pag_get_page, pagination_range = make_pagination(request, recipes, 3)
     return render(request, 'recipe/pages/search.html', context={
         'page_title': f'Buscado por "{search}"',
         'search_term': search,
-        'recipes': recipes,
+        'recipes': pag_get_page,
+        'pagination_range': pagination_range,
+        'additional_query_url': f'&q={search}'
     })
--- utils/test_func_pagination.py
+++ utils/test_func_pagination.py
@@ -1,16 +1,21 @@
 import math
 
+from django.core.paginator import Paginator
+
 
 def make_pagination_range(page_range, qty_pages, current_page):
     middle_page = math.ceil(qty_pages / 2)
     start_range = current_page - middle_page
     start_range_offset = abs(start_range) if start_range < 0 else 0
-    stop_range = current_page + middle_page + start_range_offset
+    stop_range = current_page + middle_page
     final_page = len(page_range)
     if start_range < 0:
         start_range = 0
-    if (start_range + (middle_page+1)) >= final_page:
-        start_range = final_page - qty_pages
+        stop_range += start_range_offset
+    if final_page < stop_range:
+        pass  # noqa: E501 this is equal to use start_range = 0, using this to prevent bugs
+    elif stop_range >= final_page:
+        start_range = start_range - abs(final_page - stop_range)
     pagination = page_range[start_range:stop_range]
     return {
         'pagination': pagination,
@@ -20,6 +25,30 @@ def make_pagination_range(page_range, qty_pages, current_page):
         'total_pages': final_page,
         'start_range': start_range,
         'stop_range': stop_range,
-        'first_page_out_of_range': current_page > middle_page,
+        'first_page_out_of_range': current_page >= (qty_pages - 1),  # noqa: E501
+        'first_page_active_span': current_page >= (qty_pages),  # noqa: E501
         'last_page_out_of_range': stop_range < final_page,
     }
+
+
+# make_pagination_range(
+#     page_range=list(range(1, 4)),
+#     qty_pages=4,
+#     current_page=1
+# )['pagination']
+
+
+def make_pagination(request, queryset, per_page, qty_page=4):
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+    pagination = Paginator(queryset, per_page)
+    pag_get_page = pagination.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        pagination.page_range,
+        qty_page,
+        current_page,
+    )
+    return pag_get_page, pagination_range
