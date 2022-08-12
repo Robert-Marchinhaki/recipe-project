5254e220814a5635f7858e45781511b91b7b28d9
Robert <adrianrobert288@gmail.com>
2022-08-12 13:12:47 -0300

    Configurado forms
    	- labels, widgets, error_messages, help_texts

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,35 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password'
         ]
+        labels = {
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'username': 'Username',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+        help_texts = {
+            'email': 'Please enter a valid email.'
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field is required',
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'placeholder': 'Type your last name here'
+            }),
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Type your username here'
+            }),
+            'email': forms.TextInput(attrs={
+                'placeholder': 'Type your email here'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            }),
+        }
