10d943811545203ae9d1014ca46458bfb453b230
Robert <adrianrobert288@gmail.com>
2022-08-12 14:13:55 -0300

    Sobrescrevendo os campos do formulário

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,38 @@
 from django import forms
 from django.contrib.auth.models import User
 
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
 
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['first_name'], 'Ex.: Michael')
+        add_placeholder(self.fields['last_name'], 'Ex.: bown')
+        add_placeholder(self.fields['username'], 'Ex.: michael_bown')
+        add_placeholder(self.fields['email'], 'Your e-mail')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput({
+            'placeholder': 'Enter a password'
+        }),
+        error_messages={
+            'required': 'This field is required'
+        },
+        help_text='The length of your password must be greater than 8 and must contain an uppercase letter, a lowercase letter and a numbers'
+    )
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput({
+            'placeholder': 'Repeat your password'
+        }),
+        error_messages={
+            'required': 'This field is required'
+        },
+    )
+
     class Meta:
         model = User
         fields = [
