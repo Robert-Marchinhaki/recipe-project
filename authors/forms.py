6dc3a0ab48dc9b834a8440c25ae72c112175f1b8
Robert <adrianrobert288@gmail.com>
2022-08-15 13:46:19 -0300

    Utilizado validators para validação de dados

--- authors/forms.py
+++ authors/forms.py
@@ -1,13 +1,40 @@
+from cgitb import strong
+import re
+
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
+
 def add_attr(field, attr_name, attr_new_val):
     field.widget.attrs[attr_name] = f'{attr_new_val}'.strip()
+
 
 def add_placeholder(field, placeholder_val):
     field.widget.attrs['placeholder'] = placeholder_val
     add_attr(field, 'placeholder', placeholder_val)
+
+
+def strong_password(password):
+    regex = re.compile(r'''
+    ^               # must be start with this
+    (?=.*[a-z])     # must contain one lowercase letter
+    (?=.*[A-Z])     # must contain one uppercase letter
+    (?=.*[0-9])     # must contain one number
+    (?=.*[$*&@#])   # must contain one special character
+    .{8,}           # must be greater than or equal to 8
+    $               # must be end this
+    ''', re.VERBOSE)
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='invalid'
+        )
+
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -25,7 +52,8 @@ class RegisterForm(forms.ModelForm):
         error_messages={
             'required': 'This field is required'
         },
-        help_text='The length of your password must be greater than 8 and must contain an uppercase letter, a lowercase letter and a numbers.'
+        help_text='The length of your password must be greater than 8 and must contain an uppercase letter, a lowercase letter and a numbers.',
+        validators=[strong_password]
     )
 
     password2 = forms.CharField(
@@ -36,6 +64,7 @@ class RegisterForm(forms.ModelForm):
         error_messages={
             'required': 'This field is required'
         },
+        validators=[strong_password]    
     )
 
     class Meta:
@@ -79,10 +108,10 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Type your password here'
             }),
         }
-    # data.lower() == data
+
     def clean_password(self):
         data = self.cleaned_data.get('password')
-        
+
         if len(data) < 8:
             raise ValidationError(
                 'Your password is too short.',
@@ -98,26 +127,26 @@ class RegisterForm(forms.ModelForm):
             )
 
         return data
-    
+
     def clean(self):
         all_data = super().clean()
 
         password = all_data.get('password')
         password2 = all_data.get('password2')
         pw_validation_error = ValidationError(
-                'passwords do not match.',
-                code='invalid',
-                )
+            'passwords do not match.',
+            code='invalid',
+        )
         if password != password2:
             raise ValidationError({
                 'password': [
                     'Password and password2 must be equal.',
                     pw_validation_error,
-                    ],
+                ],
                 'password2': [
                     'Password and password2 must be equal.',
                     pw_validation_error,
-                    ],
-                })
+                ],
+            })
 
         return all_data
