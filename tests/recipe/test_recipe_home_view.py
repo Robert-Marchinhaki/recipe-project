b1772b0906c301e5a8176e5150169e0ab9e4caf6
Robert <adrianrobert288@gmail.com>
2022-08-23 18:34:01 -0300

    Organizando arquivos de teste

--- tests/recipe/test_recipe_home_view.py
+++ tests/recipe/test_recipe_home_view.py
@@ -1,16 +1,6 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.by import By
-from utils.browser import make_chrome_browser
 
-
-class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
-    def setUp(self) -> None:
-        self.browser = make_chrome_browser()
-        return super().setUp()
-    
-    def tearDown(self) -> None:
-        self.browser.quit()
-        return super().tearDown()
+from .base import RecipeBaseFunctionalTest
 
 
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
