ba49149fefe6ac22345d5fdbccc726463cca3d1a
robe <adrianrobert288@gmail.com>
2022-10-12 13:52:50 -0300

    Criado template, url, view para edição de receitas

--- authors/urls.py
+++ authors/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from authors import views
 
 app_name = 'authors'
@@ -10,4 +11,9 @@ urlpatterns = [
     path('login/create/', views.login_create, name='login_create'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path(
+        'dashboard/recipe/<int:id>/edit',
+        views.dashboard_recipe_edit,
+        name='dashboard_recipe_edit'
+    ),
 ]
