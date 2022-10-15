b3745b4c201482f15f1957fb69707a36d949a667
robe <adrianrobert288@gmail.com>
2022-10-15 15:36:23 -0300

    Criado CRUD complet para receitas

--- authors/views.py
+++ authors/views.py
@@ -174,3 +174,20 @@ def dashboard_recipe_create(request):
             'form': form
         },
     )
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_delete(request, id):
+    recipe = Recipe.objects.filter(
+        is_published=False,
+        author=request.user,
+        pk=id,
+    ).first()
+
+    if not recipe:
+        raise Http404()
+
+    recipe.delete()
+    messages.success(request, "Recipe deleted with success.")
+
+    return redirect(reverse("authors:dashboard"))
