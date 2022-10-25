4746270c238dcd0f75d481f7457746b84629d9b3
robe <adrianrobert288@gmail.com>
2022-10-25 19:12:23 -0300

    Movido função para dentro da classe DashboardRecipe
    	- Função dashboard_recipe_edit (ainda falta alterar a função,
    pois, está aceitando apenas método get. É necessário incluir o método
    POST)

--- authors/views/dashboard.py
+++ authors/views/dashboard.py
@@ -4,8 +4,48 @@ from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.views import View
 from recipe.models import Recipe
 from utils.clear_str import cleaning_str
+
+
+class DashboardRecipe(View):
+    def get(self, request, id):
+        recipe = Recipe.objects.filter(
+            is_published=False,
+            author=request.user,
+            pk=id,
+        ).first()
+
+        if not recipe:
+            raise Http404()
+
+        form = AuthorRecipeForm(
+            data=request.POST or None,
+            files=request.FILES or None,
+            instance=recipe,
+        )
+
+        if form.is_valid():
+            # Now form is valid and you can try save
+            recipe = form.save(commit=False)    # Fake save
+
+            recipe.author = request.user
+            recipe.preparation_step_is_html = False
+            recipe.is_published = False
+
+            form.save()
+
+            messages.success(request, "You edited your recipe with success!")
+            return redirect(reverse("authors:dashboard_recipe_edit", args=(id,)))
+
+        return render(
+            request,
+            'authors/pages/dashboard_recipe.html',
+            context={
+                'form': form
+            },
+        )
 
 
 @login_required(login_url='authors:login', redirect_field_name='next')
@@ -19,45 +59,6 @@ def dashboard(request):
         'authors/pages/dashboard.html',
         context={
             'recipes': recipes,
-        },
-    )
-
-
-@login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard_recipe_edit(request, id):
-    recipe = Recipe.objects.filter(
-        is_published=False,
-        author=request.user,
-        pk=id,
-    ).first()
-
-    if not recipe:
-        raise Http404()
-
-    form = AuthorRecipeForm(
-        data=request.POST or None,
-        files=request.FILES or None,
-        instance=recipe,
-    )
-
-    if form.is_valid():
-        # Now form is valid and you can try save
-        recipe = form.save(commit=False)    # Fake save
-
-        recipe.author = request.user
-        recipe.preparation_step_is_html = False
-        recipe.is_published = False
-
-        form.save()
-
-        messages.success(request, "You edited your recipe with success!")
-        return redirect(reverse("authors:dashboard_recipe_edit", args=(id,)))
-
-    return render(
-        request,
-        'authors/pages/dashboard_recipe.html',
-        context={
-            'form': form
         },
     )
 
