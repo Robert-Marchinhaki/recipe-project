a73e8c87072a7e8258fda601f873c31eb6f5cdce
Robert <adrianrobert288@gmail.com>
2023-01-07 15:01:37 -0300

    Feito organização do arquivo settings

---
+++ core/settings/environment.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+from utils.environ import get_env_variable, parse_comma_sep_str_to_list
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
+
+ALLOWED_HOSTS: list[str] = parse_comma_sep_str_to_list(
+    get_env_variable('ALLOWED_HOST')
+)
+CSRF_TRUSTED_ORIGINS: list[str] = parse_comma_sep_str_to_list(
+    get_env_variable('CSRF_TRUSTED_ORIGINS')
+)
+
+ROOT_URLCONF = 'core.urls'
+
+WSGI_APPLICATION = 'core.wsgi.application'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
