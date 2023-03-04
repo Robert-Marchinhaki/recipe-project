faea535da58a0a9d3e4a5678a194d2cb3d0de718
robert <adrianrobert288@gmail.com>
2023-03-03 21:48:37 -0300

    Criado lógica no JS para abrir e fechar o menu

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -14,4 +14,33 @@ function my_scope() {
     }
 }
 
-my_scope()
+my_scope();
+
+(() => {
+    const btnCloseMenu = document.querySelector(".btn-close-menu");
+    const btnShowMenu = document.querySelector(".btn-show-menu");
+    const menuContainer = document.querySelector(".menu-container");
+
+    const btnShowMenuVisibleClass = "btn-show-menu-visible";
+    const menuHiddenClass = "menu-hidden";
+
+    const closeMenu = () => {
+        btnShowMenu.classList.add(btnShowMenuVisibleClass);
+        menuContainer.classList.add(menuHiddenClass);
+    };
+
+    const showMenu = () => {
+        btnShowMenu.classList.remove(btnShowMenuVisibleClass);
+        menuContainer.classList.remove(menuHiddenClass);
+    };
+
+    if (btnCloseMenu) {
+        btnCloseMenu.removeEventListener('click', closeMenu);
+        btnCloseMenu.addEventListener('click', closeMenu);  
+    };
+
+    if (btnShowMenu) {
+        btnShowMenu.removeEventListener('click', showMenu);
+        btnShowMenu.addEventListener('click', showMenu);
+    };
+})();
