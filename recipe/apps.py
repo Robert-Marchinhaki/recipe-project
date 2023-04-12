d38caa82b8159d9ac34a5965d3a28c31e80d2f92
robert <adrianrobert288@gmail.com>
2023-04-12 15:18:02 -0300

    feat: Feito signal para apagar imagens
    	# Ao deletar uma receita, a imagem também será apagada

--- recipe/apps.py
+++ recipe/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class RecipeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipe'
+
+    def ready(self, *args, **kwargs) -> None:
+        import recipe.signals  # noqa
+        super_ready = super().ready(*args, **kwargs)
+        return super_ready
---
+++ recipe/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+from recipe.models import Recipe
+import os
+
+
+def delete_cover(instance):
+    try:
+        os.remove(instance.cover.path)
+    except (ValueError, FileNotFoundError):
+        ...
+
+
+@receiver(pre_delete, sender=Recipe)
+def recipe_cover_delete(sender, instance, *args, **kwargs):
+    old_instance = Recipe.objects.get(pk=instance.pk)
+    delete_cover(old_instance)
